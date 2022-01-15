f5e977c5dc2dfd87b2d9b6aeb73dec98c4fd1aa1
Rahul Goswami <yuvrajiro@gmail.com>
2022-01-16 00:53:52 +0530

    Added setup.py file

---
+++ A Simple JAX based Neural Network/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='jaxNN',
+    version='0.1',
+    description='A Simple JAX based Neural Network',
+    author='Rahul Goswami',
+    author_email='yuvrajiro@gmail.com',
+    license='MIT',
+    packages=['jaxNN'],
+    zip_safe=False)
